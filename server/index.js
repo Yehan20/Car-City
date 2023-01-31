8abdbd398817f1ff2fc2fa94a168a257775e38ab
Yehan20 <yehansk15@gmail.com>
2023-01-31 16:14:04 +0530

    .

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import adminRoutes from "./routes/admin.route.js";
 import cors from 'cors'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
 
-const KEY = dotenv.config().parsed.CONNECTION_URL;
+// const KEY = dotenv.config().parsed.CONNECTION_URL;
 const app = express();
-//const url = 'mongodb+srv://yehan:yehan1234@carcitycluster.dodfqtz.mongodb.net/CarCity?retryWrites=true&w=majority'
+const url = 'mongodb+srv://yehan:yehan1234@carcitycluster.dodfqtz.mongodb.net/CarCity?retryWrites=true&w=majority'
 
 
 app.use(express.urlencoded({extended:true}));
@@ -22,7 +22,7 @@ app.use('/admin',adminRoutes)
 
 app.listen(3001, () => {
     mongoose.set('strictQuery', false);
-    mongoose.connect(KEY, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
         console.log('Connetect');
     })
 
